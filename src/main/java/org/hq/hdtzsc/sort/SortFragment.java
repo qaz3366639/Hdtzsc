9643109ef64f90038c6050798be127c216ab5ec9
WuRuiQ <263454190@qq.com>
2015-05-31 19:41:28 +0800

    下拉刷新

--- src/main/java/org/hq/hdtzsc/sort/SortFragment.java
+++ src/main/java/org/hq/hdtzsc/sort/SortFragment.java
@@ -8,6 +8,10 @@ import android.view.ViewGroup;
 
 import org.hq.hdtzsc.R;
 
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.PtrHandler;
+import in.srain.cube.views.ptr.header.MaterialHeader;
+
 /**
  * Description:
  * Author: WuRuiqiang(263454190@qq.com)
@@ -15,9 +19,38 @@ import org.hq.hdtzsc.R;
  */
 public class SortFragment extends Fragment {
 
+    protected PtrFrameLayout mPtrFrameLayout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sort, container, false);
+        mPtrFrameLayout = (PtrFrameLayout) view.findViewById(R.id.store_house_ptr_frame);
+        // header
+        final MaterialHeader header = new MaterialHeader(getActivity());
+        header.setLayoutParams(new PtrFrameLayout.LayoutParams(-1, -2));
+        header.setPadding(0, 20, 0, 20);
+        header.setPtrFrameLayout(mPtrFrameLayout);
+
+        mPtrFrameLayout.setLoadingMinTime(1000);
+        mPtrFrameLayout.setDurationToCloseHeader(1500);
+        mPtrFrameLayout.setHeaderView(header);
+        mPtrFrameLayout.addPtrUIHandler(header);
+        mPtrFrameLayout.setPtrHandler(new PtrHandler() {
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout ptrFrameLayout, View view, View view1) {
+                return true;
+            }
+
+            @Override
+            public void onRefreshBegin(final PtrFrameLayout ptrFrameLayout) {
+                ptrFrameLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ptrFrameLayout.refreshComplete();
+                    }
+                }, 1000);
+            }
+        });
         return view;
     }
 }
